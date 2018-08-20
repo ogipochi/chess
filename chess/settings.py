2850902e9db688c3df17e42059ff969bc17064bb
ogipochi <pochi.0226.ogi@gmail.com>
2018-08-20 12:05:51 +0900

    requirements.txt

--- chess/settings.py
+++ chess/settings.py
@@ -140,7 +140,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('10.0.0.3', 6379)],
+            "hosts": [('localhost', 6379)],
         },
     },
 }
